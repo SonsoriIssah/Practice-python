cd62cf3ff2b626151f62692e5e1bd8b0bedf71ac
SonsoriIssah <issonsori@gmail.com>
2026-04-30 01:12:04 +0000

    Extras

--- Character_input.py
+++ Character_input.py
@@ -1,5 +1,8 @@
 name = input('What is your name? ')
 age = int(input('What is your age? '))
 years_left =(100-age)+2026
-
+random = int(input('Enter a random number: '))
 print(f'Hello {name}, you will turn 100 years in {years_left} years')
+
+for i in range(random):
+    print(f'Hello {name}, you will turn 100 years in {years_left} years')
